4a85451c6e02ab0e0826c79edbaafcaf06972ebe
nehasinsghn <97651021+nehasinsghn@users.noreply.github.com>
2022-05-09 17:32:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evenodd
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("enter a number");
+            
+            for (int i=0; i <= 5; i++)
+            {
+                for(int j=0; j<=10;j++)
+                Console.WriteLine("*");
+
+            }
+           
+            
+            
+               
+       
+            Console.WriteLine("for loop terminate");
+            Console.ReadLine();
+            }
+        } 
+    }
+
